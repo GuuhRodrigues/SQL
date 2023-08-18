44f3940fedc1c504ad09fafe4a640344609bd366
Gustavo Rodrigues <102631537+GuuhRodrigues@users.noreply.github.com>
2023-08-17 21:02:12 -0300

    Delete ex3.sql

--- ex3.sql
+++
@@ -1,7 +0,0 @@
-# Atualize o endereço do cliente com cpf 19290992743 para R. Jorge Emílio 23 o bairro para Santo Amaro, a cidade para São Paulo, o estado para SP e o CEP para 8833223.
-
-SELECT * FROM clientes WHERE CPF = '19290992743';
-
-UPDATE clientes SET ENDERECO = 'R. Jorge Emílio 23', BAIRRO = 'Santo Amaro', CIDADE = 'São Paulo', ESTADO = 'SP', CEP = '8833223';
-
-
